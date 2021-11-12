61ce6a2d0fbb723a7c3b270543b6ba58a40ba3c1
hvroberta89 <veber.roberta@gmail.com>
2021-11-12 01:06:10 +0100

    completed tasks

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,13 @@
-
+const setCookie= (value) => {
+  const now= new Date();
+  now.setTime(now.getTime() + (15*60*1000));
+  const expires = now.toUTCString();  //a kapott időt átalakítjuk UTC stringé
+  document.cookie = `token=${value};expires=${expires};path=/`;
+}
 
 
 
 
 
 // export default setCookie;
+export default setCookie;
